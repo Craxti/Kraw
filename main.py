b8b0c1bab3c7912a4ceac72d066fadd9f9b03457
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2023-02-28 22:33:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import requests
 import time
 import statistics
 import logging
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 def setup_logger():
     logging.basicConfig(
@@ -35,7 +37,15 @@ def print_stats(num_requests, success_set, failure_set, elapsed_time):
         logging.info(f"Slowest time: {max(success_set):.2f} seconds")
         logging.info(f"Average time: {statistics.mean(success_set):.2f} seconds")
 
-def run_test(url, num_requests, num_threads, test_time):
+def plot_results(success_set, failure_set):
+    labels = ['Success', 'Failure']
+    sizes = [len(success_set), len(failure_set)]
+    colors = ['#00ff00', '#ff0000']
+    plt.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%', startangle=90)
+    plt.axis('equal')
+    plt.show()
+
+def run_test(url, num_requests, num_threads, test_time, plot=False):
     session = requests.Session()
     session.headers.update({'User-Agent': 'Mozilla/5.0'})
     results_queue = concurrent.futures.Queue()
@@ -47,9 +57,11 @@ def run_test(url, num_requests, num_threads, test_time):
         for i in range(num_requests):
             executor.submit(send_request, session, url, results_queue)
 
+        progress_bar = tqdm(total=num_requests * num_threads)
         while time.monotonic() - start_time < test_time:
             try:
                 result = results_queue.get(timeout=1)
+                progress_bar.update(1)
                 if result:
                     success_set.add(result)
                 else:
@@ -57,8 +69,13 @@ def run_test(url, num_requests, num_threads, test_time):
             except concurrent.futures.TimeoutError:
                 pass
 
+        progress_bar.close()
+
     elapsed_time = time.monotonic() - start_time
     print_stats(num_requests * num_threads, success_set, failure_set, elapsed_time)
+
+    if plot:
+        plot_results(success_set, failure_set)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Website stress testing tool')
