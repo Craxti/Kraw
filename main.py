752e86e83a51cb783a3dfd6b0f119491fbd453a2
Aleksandr Fetisov <55095407+Craxti@users.noreply.github.com>
2023-02-28 21:56:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,102 +1,60 @@
+import argparse
+import concurrent.futures
 import requests
-import threading
-import queue
-from typing import List, Tuple
-import csv
-import validators
+import time
+import statistics
 
-NUM_THREADS = 10  # количество потоков
-NUM_REQUESTS = 100  # количество запросов
-TIMEOUT = 5  # таймаут для запросов в секундах
-URLS_FILE = 'urls.txt'  # файл со списком URL-адресов
-PROXIES = {'http': 'http://localhost:8080', 'https': 'http://localhost:8080'}  # прокси-сервер
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+def send_request(session, url):
+    try:
+        response = session.get(url)
+        return response.status_code
+    except requests.exceptions.RequestException:
+        return None
 
-class RequestWorker(threading.Thread):
-    """Класс для работы с запросами в отдельном потоке"""
-    def __init__(self, queue: queue.Queue, result_queue: queue.Queue, headers: dict):
-        super().__init__()
-        self.queue = queue
-        self.result_queue = result_queue
-        self.headers = headers
+def print_stats(num_requests, success_set, failure_set, elapsed_time):
+    num_success = len(success_set)
+    num_failure = len(failure_set)
+    success_rate = num_success / num_requests * 100
+    print(f"\nElapsed time: {elapsed_time:.2f} seconds")
+    print(f"Total requests: {num_requests}")
+    print(f"Successful requests: {num_success} ({success_rate:.2f}%)")
+    print(f"Failed requests: {num_failure} ({100 - success_rate:.2f}%)")
+    if num_success > 0:
+        print(f"Fastest time: {min(success_set):.2f} seconds")
+        print(f"Slowest time: {max(success_set):.2f} seconds")
+        print(f"Average time: {statistics.mean(success_set):.2f} seconds")
 
-    def run(self) -> None:
-        while True:
-            # Получаем URL из очереди
-            url = self.queue.get()
+def run_test(url, num_requests, num_threads, test_time):
+    session = requests.Session()
+    session.headers.update({'User-Agent': 'Mozilla/5.0'})
+    results_queue = concurrent.futures.Queue()
+    success_set = set()
+    failure_set = set()
+    start_time = time.monotonic()
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
+        for i in range(num_requests):
+            executor.submit(send_request, session, url, results_queue)
+
+        while time.monotonic() - start_time < test_time:
             try:
-                # Проверяем формат URL-адреса
-                if not validators.url(url):
-                    raise ValueError(f"Invalid URL format: {url}")
-                # Отправляем запрос
-                response = requests.get(url, timeout=TIMEOUT, proxies=PROXIES, headers=HEADERS)
-                # Добавляем результат в очередь результатов
-                self.result_queue.put((url, response.status_code))
-            except (requests.exceptions.RequestException, ValueError) as e:
-                # Если произошла ошибка, добавляем None в очередь результатов
-                self.result_queue.put((url, None, str(e)))
-            finally:
-                # Уменьшаем значение счетчика
-                self.queue.task_done()
+                result = results_queue.get(timeout=1)
+                if result:
+                    success_set.add(result)
+                else:
+                    failure_set.add(result)
+            except concurrent.futures.TimeoutError:
+                pass
 
-import csv
+    elapsed_time = time.monotonic() - start_time
+    print_stats(num_requests * num_threads, success_set, failure_set, elapsed_time)
 
-def stress_test(output_file):
-    # Создаем очереди для URL-адресов и результатов
-    urls_queue = Queue()
-    result_queue = Queue()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Website stress testing tool')
+    parser.add_argument('url', type=str, help='URL of the website to test')
+    parser.add_argument('--requests', '-r', type=int, default=1000, help='Number of requests to send')
+    parser.add_argument('--threads', '-t', type=int, default=10, help='Number of threads to use')
+    parser.add_argument('--time', '-s', type=int, default=60, help='Duration of the test in seconds')
+    args = parser.parse_args()
 
-    # Заполняем очередь URL-адресами из файла
-    with open(URLS_FILE) as f:
-        urls = f.read().splitlines()
-    for url in urls:
-        urls_queue.put(url)
-
-    # Запускаем потоки-работники
-    for i in range(NUM_THREADS):
-        t = RequestWorker(urls_queue, result_queue)
-        t.daemon = True
-        t.start()
-
-    # Ждем, пока все URL будут обработаны
-    urls_queue.join()
-
-    # Получаем результаты из очереди результатов
-    results = []
-    while not result_queue.empty():
-        results.append(result_queue.get())
-
-    # Сортируем результаты по URL-адресам
-    results = sorted(results, key=lambda x: x[0])
-
-    # Выводим результаты в консоль и записываем в файл
-    with open(output_file, 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['URL', 'Status Code'])
-        for url, status_code in results:
-            if status_code is None:
-                print(f"{url}: Request failed")
-                writer.writerow([url, 'Request failed'])
-            else:
-                print(f"{url}: Status code {status_code}")
-                writer.writerow([url, status_code])
-
-
-if __name__ == '__main__':
-    # Читаем URL-адреса из файла
-    with open(URLS_FILE) as f:
-        urls = f.read().splitlines()
-
-    # Запускаем тестирование
-    results = stress_test(urls)
-
-    # Выводим результаты
-    for url, status_code in results:
-        if status_code is None:
-            print(f"{url}: Request failed")
-        else:
-            print(f"{url}: Status code {status_code}")
-    
-    output_file = 'results.csv'
-    stress_test(output_file)
-    print(f'Results saved to {output_file}')
+    run_test(args.url, args.requests, args.threads, args.time)
